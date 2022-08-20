afc2be723f9af491e48cdca0688d10e29416c310
NikolajTrenirovki <nikolya.semenov.1997@list.ru>
2022-08-20 17:00:10 +0300

    created a structure

---
+++ app/src/main/java/com/sem/exchangerate/presentation/App.kt
@@ -0,0 +1,6 @@
+package com.sem.exchangerate.presentation
+
+import android.app.Application
+
+class App: Application() {
+}
---
+++ app/src/main/java/com/sem/exchangerate/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.sem.exchangerate.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sem.exchangerate.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
