1d9a58150704c51d1779c13afcf1d29c8f635341
Nikolui333 <semenovnikolu@yandex.ru>
2022-08-21 13:12:00 +0300

    fixed bug A failure occurred while executing com.android.build.gradle.internal.res.ParseLibraryResourcesTask$ParseResourcesRunnable

--- app/src/main/java/com/sem/exchangerate/data/localDB/ExchangeRateDB.kt
+++ app/src/main/java/com/sem/exchangerate/data/localDB/ExchangeRateDB.kt
@@ -1,6 +1,8 @@
 package com.sem.exchangerate.data.localDB
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.sem.exchangerate.data.models.ExchangeRateModel
 import com.sem.exchangerate.data.models.FavouriteModel
@@ -8,4 +10,22 @@ import com.sem.exchangerate.data.models.FavouriteModel
 @Database(entities = [ExchangeRateModel::class, FavouriteModel::class], version = 1)
 abstract class ExchangeRateDB : RoomDatabase() {
 
+    companion object { // аналог static в java
+        @Volatile
+        private var INSTANCE: ExchangeRateDB? = null
+
+        fun getDatabase(context: Context): ExchangeRateDB {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context,
+                    ExchangeRateDB::class.java,
+                    "exchangeRateDB")
+                    .createFromAsset("database/exchangeRateDB.db")
+                    .build()
+                INSTANCE = instance
+
+                instance
+            }
+        }
+    }
 }
--- app/src/main/java/com/sem/exchangerate/presentation/MainActivity.kt
+++ app/src/main/java/com/sem/exchangerate/presentation/MainActivity.kt
@@ -2,11 +2,35 @@ package com.sem.exchangerate.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.sem.exchangerate.R
+import com.sem.exchangerate.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        // запуск фрагмента Home (в данном случае он является фрагментом по умолчанию)
+        supportFragmentManager.beginTransaction().replace(R.id.mainContent, ExchangeRateFragment()).commit()
+
+        setSupportActionBar(binding?.topMainMenu)
+
+        // обработчик нажатий по вкладнкам
+        binding?.bottomMainMenu?.setOnItemSelectedListener { item ->
+
+            when(item.itemId) {
+                R.id.rateBottomMainMenu -> supportFragmentManager.beginTransaction().replace(R.id.mainContent, ExchangeRateFragment()).commit()
+                R.id.favoriteBottomMainMenu -> supportFragmentManager.beginTransaction().replace(R.id.mainContent, FavouriteFragment()).commit()
+            }
+
+            return@setOnItemSelectedListener true
+
+        }
+        binding?.bottomMainMenu?.selectedItemId = R.id.rateBottomMainMenu
     }
 }
