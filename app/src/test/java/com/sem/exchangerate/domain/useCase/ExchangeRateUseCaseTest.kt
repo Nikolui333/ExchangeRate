716a6d8996330e6055e6a8e54c968f51317eb0dd
Nikolui333 <56168199+Nikolui333@users.noreply.github.com>
2022-09-02 17:53:15 +0300

    unit tests work correctly

--- app/src/test/java/com/sem/exchangerate/domain/useCase/ExchangeRateUseCaseTest.kt
+++ app/src/test/java/com/sem/exchangerate/domain/useCase/ExchangeRateUseCaseTest.kt
@@ -1,9 +1,11 @@
 package com.sem.exchangerate.domain.useCase
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.MutableLiveData
 import com.sem.exchangerate.data.models.ExchangeRateModel
 import com.sem.exchangerate.domain.repository.ExchangeRateCall
 import org.junit.Assert
+import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mockito
 import org.mockito.kotlin.mock
@@ -11,6 +13,10 @@ import org.mockito.kotlin.mock
 class ExchangeRateUseCaseTest {
 
     val userRepository = mock<ExchangeRateCall>()
+
+    @Rule
+    @JvmField
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
 
     @Test
     fun loadExchangeRate() {
@@ -27,14 +33,12 @@ class ExchangeRateUseCaseTest {
         val liveData2 = MutableLiveData<List<ExchangeRateModel>>()
         liveData2.value = listOf<ExchangeRateModel>(ExchangeRateModel(0, "name", 0.34), ExchangeRateModel(1, "name2", 0.35))
 
-        Assert.assertEquals(liveData2, actual)
-    }
-
-    @Test
-    fun test1() {
-
-        Assert.assertEquals(4, 2 + 2)
-
+        Assert.assertEquals(liveData2.value!!.get(0).id, actual.value!!.get(0).id)
+        Assert.assertEquals(liveData2.value!!.get(0).name, actual.value!!.get(0).name)
+        Assert.assertEquals(liveData2.value!!.get(0).exchange, actual.value!!.get(0).exchange)
+        Assert.assertEquals(liveData2.value!!.get(1).id, actual.value!!.get(1).id)
+        Assert.assertEquals(liveData2.value!!.get(1).name, actual.value!!.get(1).name)
+        Assert.assertEquals(liveData2.value!!.get(1).exchange, actual.value!!.get(1).exchange)
     }
 
 }
