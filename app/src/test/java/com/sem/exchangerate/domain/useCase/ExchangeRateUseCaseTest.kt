0fd83300cd3640da8ed2d602d11b606297c4d66a
Nikolui333 <56168199+Nikolui333@users.noreply.github.com>
2022-09-02 12:34:17 +0300

    unit testing start

---
+++ app/src/test/java/com/sem/exchangerate/domain/useCase/ExchangeRateUseCaseTest.kt
@@ -0,0 +1,15 @@
+package com.sem.exchangerate.domain.useCase
+
+import org.junit.Assert
+import org.junit.Test
+
+class ExchangeRateUseCaseTest {
+
+    @Test
+    fun test1() {
+
+        Assert.assertEquals(4, 2 + 2)
+
+    }
+
+}
